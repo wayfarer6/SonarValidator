e63064ad2fc493f2a46ef9d46183422ed036d36f
wayfarer6 <shseo2023@gmail.com>
2026-09-06 10:41:49 +0900

    save

---
+++ SonarValidar_Backend/SonarValidar_Backend/src/main/java/org/sonar_validator/sonarvalidar_backend/SonarValidarBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sonar_validator.sonarvalidar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarValidarBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonarValidarBackendApplication.class, args);
+    }
+
+}
---
+++ SonarValidar_Backend/SonarValidar_Backend/src/test/java/org/sonar_validator/sonarvalidar_backend/SonarValidarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonar_validator.sonarvalidar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarValidarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
