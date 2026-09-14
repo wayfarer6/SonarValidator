e5feadc1c5ea8cea807e80eae0ffd0806304338e
wayfarer6 <shseo2023@gmail.com>
2026-09-14 14:49:07 +0900

    save

--- SonarValidator_Frontend/src/components/header/UserDropdown.tsx
+++ SonarValidator_Frontend/src/components/header/UserDropdown.tsx
@@ -34,9 +34,7 @@ export default function UserDropdown() {
         onClick={toggleDropdown}
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
-        <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <img src="/images/user/owner.jpg" alt="User" />
-        </span>
+
 
         <span className="block mr-1 font-medium text-theme-sm">{displayName}</span>
         <svg
