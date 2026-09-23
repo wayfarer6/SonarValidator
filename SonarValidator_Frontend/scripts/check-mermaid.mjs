5950ff377463731f2515af6d9df71eefaecce47d
wayfarer6 <shseo2023@gmail.com>
2026-09-23 16:55:30 +0900

    [SONAR-6] Jira 연동

--- SonarValidator_Frontend/scripts/check-mermaid.mjs
+++ SonarValidator_Frontend/scripts/check-mermaid.mjs
@@ -3,7 +3,7 @@ import { JSDOM } from "jsdom";
 
 const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>", {
   pretendToBeVisual: true,
-  url: "http://localhost/",
+  url: "http://localhost/",  
 });
 globalThis.window = dom.window;
 globalThis.document = dom.window.document;
